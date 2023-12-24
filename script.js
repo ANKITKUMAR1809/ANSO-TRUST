54d3f754b4a0bb3a1ff1910b686381ff230b69d3
ANKITKUMAR1809 <skwedsak143@gmail.com>
2023-12-25 02:01:37 +0530

    file added for donation box

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("show image is working")
-let elem=document.getElementById("showImg");
-elem.style.display="none";
-
-function showImage() {
-    document.getElementById("showimg").style.display="block";
-}
